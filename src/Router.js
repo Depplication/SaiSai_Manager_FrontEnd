3a1722da9b49b97848a11ef2b14df7faf8349638
smile0902 <rladjstn@dgsw.hs.kr>
2023-09-06 23:16:50 +0900

    ⚡️ :: 기능 업데이트

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OwnerMngmnPage from "../src/pages/OwnerMngmn/OwnerMngmn";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<></>}></Route>
+        <Route exact path="/ownermngmn" element={<OwnerMngmnPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
