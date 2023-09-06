eb440f1d33834b995946d8a7859f128785f8634c
smile0902 <rladjstn@dgsw.hs.kr>
2023-09-06 09:30:15 +0900

    📄 :: 파일 생성

---
+++ src/pages/OwnerMngmn/OwnerMngme.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as O from "../../styles/OwnerMngmn/OwnerMngmn.style";
+
+function OwnerMngmn() {
+  return (
+    <>
+      <O.div>회원 관리 업주</O.div>
+    </>
+  );
+}
+
+export default OwnerMngmn;
