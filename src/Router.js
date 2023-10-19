cd20c9c601f65160706743d80e75eb93fba3615c
smile0902 <rladjstn@dgsw.hs.kr>
2023-10-19 21:58:46 +0900

    Revert "Merge pull request #4 from joshmoon827/companyIncome"
    This reverts commit 04666ce6dc03c4868ba0a42fbea27f5823261bde, reversing
    changes made to d412e0ca5955d1f6ac50bb1dfef83ee115e87212.

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import LoginPage from "./pages/Login/Login";
 import LoginPage from "./pages/Login/LoginPage";
-import PointHistoryPage from "./pages/PointHistory/PointHistory";
 
 function Router() {
   return (
@@ -10,7 +9,6 @@ function Router() {
       <Routes>
         <Route path="/" element={<h1>hihi</h1>} />
         <Route exact path="/login" element={<LoginPage />}></Route>
-        <Route exact path="/pointHistory" element={<PointHistoryPage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
