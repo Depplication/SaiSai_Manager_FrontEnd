ee7397f10ba6dd78b313f809b6db36a09a2c12c7
gye0n1 <1230rbdus@gmail.com>
2023-10-31 08:39:46 +0900

    header

---
+++ src/styles/Payment/PaymentHistory.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const pointHistoryBox = styled.div`
+  padding: 8px 64px;
+  gap: 10px;
+  border-bottom: 1px solid var(--gray-diongray-250, #8e8e8f);
+  position: absolute;
+  margin: 128px 1041px 910px 200px;
+`;
+
+export const pointHistory = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+  text-align: center;
+
+  /* Web/Regular/Body 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+`;
+
+export const couponHistoryBox = styled.div`
+  padding: 8px 64px;
+  gap: 10px;
+  border-bottom: 1px solid var(--main-dionpink-300, #ff6362);
+  position: absolute;
+  margin: 128px 856px 910px 399px ;
+`;
+
+
+export const couponHistory = styled.div`
+  color: var(--main-dionpink-300, #ff6362); 
+   text-align: center; 
+
+  // Web/Regular/Body 2 
+  /* font-family: Pretendard; */
+  /* font-size: 16px;
+  font-style: normal; */
+  //font-weight: 500;
+  //line-height: 26px; //162.5% 
+  /* letter-spacing: -0.6px; */ 
+`;
