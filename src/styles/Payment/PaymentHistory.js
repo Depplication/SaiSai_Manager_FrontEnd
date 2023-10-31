e9a220f04172b8b7374169a5b1dae4243a84f3b7
gye0n1 <1230rbdus@gmail.com>
2023-10-31 20:42:50 +0900

    complete

--- src/styles/Payment/PaymentHistory.js
+++ src/styles/Payment/PaymentHistory.js
@@ -32,7 +32,6 @@ export const couponHistoryBox = styled.div`
 export const couponHistory = styled.div`
   color: var(--main-dionpink-300, #ff6362);
   text-align: center;
-  
 
   // Web/Regular/Body 2
   /* font-family: Pretendard; */
@@ -55,7 +54,6 @@ export const num = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 216px;
   position: absolute;
-
 `;
 
 export const representative = styled.div`
@@ -70,7 +68,6 @@ export const representative = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 285px;
   position: absolute;
-
 `;
 
 export const store = styled.div`
@@ -85,7 +82,6 @@ export const store = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 374px;
   position: absolute;
-
 `;
 
 export const phoneNum = styled.div`
@@ -100,7 +96,6 @@ export const phoneNum = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 476px;
   position: absolute;
-
 `;
 
 export const amount = styled.div`
@@ -115,7 +110,6 @@ export const amount = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 634px;
   position: absolute;
-
 `;
 
 export const bank = styled.div`
@@ -130,7 +124,6 @@ export const bank = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 747px;
   position: absolute;
-
 `;
 
 export const accountNum = styled.div`
@@ -145,7 +138,6 @@ export const accountNum = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 813px;
   position: absolute;
-
 `;
 
 export const paymentDate = styled.div`
@@ -160,4 +152,257 @@ export const paymentDate = styled.div`
   letter-spacing: -0.6px;
   margin: 234px 0px 0px 982px;
   position: absolute;
+`;
+
+export const ex = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  margin: 268px 0px 0px 200px;
+  position: absolute;
+`;
+
+export const exNum = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 16px;
+  position: absolute;
+`;
+
+export const exRepresentative = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 85px;
+  position: absolute;
+`;
+
+export const exStore = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 174px;
+  position: absolute;
+`;
+
+export const exPhoneNum = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 276px;
+  position: absolute;
+`;
+
+export const exAmount = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 438px;
+  position: absolute;
+`;
+
+export const exBank = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 537px;
+  position: absolute;
+`;
+
+export const exAccountNum = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 613px;
+  position: absolute;
+`;
+
+export const exPaymentDate = styled.div`
+  color: var(--gray-diongray-400, #404040);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 782px;
+  position: absolute;
+`;
+
+export const success = styled.div`
+  color: var(--etc-dionblue, #4b88ff);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 996px;
+  position: absolute;
+`;
+
+export const ex2 = styled.div`
+  margin: 340px 0px 0px 200px;
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+`;
+
+export const ex3 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 412px 0px 0px 200px;
+`;
+
+export const fail = styled.div`
+  color: var(--etc-dionred, #f44355);
+
+  /* Web/Bold/Subhead 2 */
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.6px;
+  margin-left: 996px;
+`;
+
+export const ex4 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 484px 0px 0px 200px;
+`;
+
+export const ex5 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 556px 0px 0px 200px;
+`;
+
+export const ex6 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 628px 0px 0px 200px;
+`;
+
+export const ex7 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 700px 0px 0px 200px;
+`;
+
+
+export const ex8 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 772px 0px 0px 200px;
+`;
+
+
+export const ex9 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 844px 0px 0px 200px;
+`;
+
+
+export const ex10 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 916px 0px 0px 200px;
+`;
+
+
+export const ex11 = styled.div`
+  width: 1040px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--essential-dionwhite, #fff);
+  position: absolute;
+  margin: 988px 0px 0px 200px
 `;
