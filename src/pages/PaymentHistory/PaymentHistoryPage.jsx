56bf5b50196ea0f3fc85f1bf7dfab63c4356a827
gye0n1 <1230rbdus@gmail.com>
2023-10-31 17:03:20 +0900

    .

--- src/pages/PaymentHistory/PaymentHistoryPage.jsx
+++ src/pages/PaymentHistory/PaymentHistoryPage.jsx
@@ -9,6 +9,17 @@ function PaymentHistory() {
       <L.couponHistoryBox>
         <L.couponHistory>쿠폰 내역</L.couponHistory>
       </L.couponHistoryBox>
+
+      <L.num>순번</L.num>
+      <L.representative>대표자 명</L.representative>
+      <L.store>가게 명</L.store>
+      <L.phoneNum>전화번호</L.phoneNum>
+      <L.amount>결제 금액</L.amount>
+      <L.bank>은행</L.bank>
+      <L.accountNum>계좌번호</L.accountNum>
+      <L.paymentDate>결제일</L.paymentDate>
+
+      
     </div>
   );
 }
--- src/styles/Payment/PaymentHistory.js
+++ src/styles/Payment/PaymentHistory.js
@@ -26,19 +26,138 @@ export const couponHistoryBox = styled.div`
   gap: 10px;
   border-bottom: 1px solid var(--main-dionpink-300, #ff6362);
   position: absolute;
-  margin: 128px 856px 910px 399px ;
+  margin: 128px 856px 910px 399px;
 `;
 
-
 export const couponHistory = styled.div`
-  color: var(--main-dionpink-300, #ff6362); 
-   text-align: center; 
+  color: var(--main-dionpink-300, #ff6362);
+  text-align: center;
+  
 
-  // Web/Regular/Body 2 
+  // Web/Regular/Body 2
   /* font-family: Pretendard; */
   /* font-size: 16px;
   font-style: normal; */
   //font-weight: 500;
-  //line-height: 26px; //162.5% 
-  /* letter-spacing: -0.6px; */ 
+  //line-height: 26px; //162.5%
+  /* letter-spacing: -0.6px; */
+`;
+
+export const num = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 216px;
+  position: absolute;
+
+`;
+
+export const representative = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 285px;
+  position: absolute;
+
+`;
+
+export const store = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 374px;
+  position: absolute;
+
+`;
+
+export const phoneNum = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 476px;
+  position: absolute;
+
+`;
+
+export const amount = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 634px;
+  position: absolute;
+
+`;
+
+export const bank = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 747px;
+  position: absolute;
+
+`;
+
+export const accountNum = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 813px;
+  position: absolute;
+
+`;
+
+export const paymentDate = styled.div`
+  color: var(--gray-diongray-250, #8e8e8f);
+
+  /* Web/Bold/Subhead 1 */
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.6px;
+  margin: 234px 0px 0px 982px;
+  position: absolute;
 `;
